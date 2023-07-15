03bde869f6aa8a76b7cb78127c909082f2f7468d
hemanth5544 <rachapalli.hemanth5544@gmail.com>
2023-07-15 16:25:55 +0530

    commit3

---
+++ nodeHealth.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+###########
+#Author: Hemanth
+#dat:15-07-2023
+#version:v1
+##########
+echo "print the disk Space"
+df -h
+
+echo"print memory"
+free -g
+
+echo"print pcHealth"
+nproc
